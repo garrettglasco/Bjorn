df4689618ab09215e24505aec3d85124db233ee0
garrrettglasco <garrett.glasco20@gmail.com>
2025-10-20 16:21:04 -0500

    Initial Commit

---
+++ Bjorn/BjornApp.swift
@@ -0,0 +1,17 @@
+//
+//  BjornApp.swift
+//  Bjorn
+//
+//  Created by gmac on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BjornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BjornTests/BjornTests.swift
@@ -0,0 +1,16 @@
+//
+//  BjornTests.swift
+//  BjornTests
+//
+//  Created by gmac on 10/20/25.
+//
+
+import Testing
+
+struct BjornTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
