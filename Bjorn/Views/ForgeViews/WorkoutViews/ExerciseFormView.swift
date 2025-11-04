04a43884b3d4e4980104f6d7c7778f068442913c
garrrettglasco <garrett.glasco20@gmail.com>
2025-11-04 12:11:54 -0600

    Create workout view

---
+++ Bjorn/Views/ForgeViews/WorkoutViews/ExerciseFormView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseFormView.swift
+//  Bjorn
+//
+//  Created by gmac on 11/4/25.
+//
+
+import SwiftUI
+
+struct ExerciseFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseFormView()
+}
